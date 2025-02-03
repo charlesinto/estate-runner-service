ad5ee2bb4b87403daf5ff6af08839dad25f8fc44
Charles <charles.onuorah@yahoo.com>
2025-02-03 14:47:36 +0100

    Initial commit

---
+++ src/test/java/com/ireveal/EstateRunner/EstateRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ireveal.EstateRunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
